0fbc7965b93def689185e186b4c2cbf3c607fd88
Grace Tan <gracetan37@gmail.com>
2019-03-31 02:24:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  var deepest = document.querySelector('#grand-node').querySelectorAll('div');
+  return deepest[deepest.length-1];
+}
