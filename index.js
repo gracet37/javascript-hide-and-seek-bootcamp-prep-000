31269c928196c96ef6ac93f16c880e511b2084b4
Grace Tan <gracetan37@gmail.com>
2019-03-31 03:07:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  var deepest = document.querySelector('#grand-node').querySelectorAll('div');
+  return deepest[deepest.length-1];
+}
+
+function increaseRankBy(n) {
+  const up = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i=0; i< up.length ; i++) {
+    up[i].innerHTML = parseInt(up[i].innerHTML) + n;
+  }
+}
